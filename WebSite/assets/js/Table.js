6c4278d4c12433a196467b2534db4ba3ef4c567c
xoxkoo <anton.durcak22@gmail.com>
2021-12-09 13:11:35 +0100

    schedule dynamically created, added changing class

--- WebSite/assets/js/Table.js
+++ WebSite/assets/js/Table.js
@@ -2,22 +2,39 @@ export default class Table {
 
   constructor( xml, Lesson ) {
     this.xml = xml
-    this.table = $('.schedule')[0]
     this.Lesson = Lesson
-    this.lessons = this.xml.children[0].children
+    this.lessons
+    this.glider
   }
 
+  /**
+   * Generate nodes and add them to html
+   */
   generateTable() {
+    this.lessons = this.xml.children[0].children
+    
     const firstLesson = new this.Lesson(this.lessons[0])
     const lastLesson = new this.Lesson(this.lessons[this.lessons.length - 1])
     let firstDay
+
+    //creating node dynamically everytime
+    const node = 
+    `<div class="glider-contain">
+      <div class="glider schedule"></div>
+      <button aria-label="Previous" class="glider-prev">«</button>
+      <button aria-label="Next" class="glider-next">»</button>
+    </div>`
+
+    $('#classSelect').after($($.parseHTML(node))[0])
+
+
 
     //looping throught weeks, creating days in html
     for(let week = firstLesson.getWeek(); week <= lastLesson.getWeek(); week++) {
 
       firstDay = this.getDateOfWeek(week, firstLesson.getYear())
 
-      this.table.innerHTML += 
+      $('.schedule')[0].innerHTML += 
       `<div class="week" data-week="${week}">
         <div class="day" data-date="${this.addDays(firstDay, 0)}"></div>
         <div class="day" data-date="${this.addDays(firstDay, 1)}"></div>
@@ -32,7 +49,7 @@ export default class Table {
   }
 
   showData() {
-    let str = ''
+    let str = '', node
 
     //looping throught every lesson, adding them to table
     Array.from(this.lessons).forEach(element => {
@@ -40,7 +57,7 @@ export default class Table {
 
       let lesson = new this.Lesson(element)
 
-      const node = $(`[data-date="${lesson.getDate()}"]`)[0]
+      node = $(`[data-date="${lesson.getDate()}"]`)[0]
     
       str += `<div class="lesson ${lesson.getCourse()}">`
       // str += '<span class="date item">' + lesson.getDate() + '</span>'
@@ -53,58 +70,98 @@ export default class Table {
       node.innerHTML += str
     })
     
+    this.createGlider()
+    this.scrollToWeek()
+    
     this.highlightToday()
+    
+  }
 
+  destroyGlider() {
+    $('.glider-contain')[0].remove()
+  }
+  
+  /**
+   * Create new Glider object, add event listener so number of week is changed everytime we change week
+   */
+  createGlider() {
     /**
      * We used external library for schedule
      * 
      * https://nickpiscitelli.github.io/Glider.js/
      */
-    const glider = new Glider(document.querySelector('.glider'), {
-      slidesToShow: 1,
+    this.glider = new Glider(document.querySelector('.schedule'),{
       slidesToScroll: 1,
+      slidesToShow: 1,
       draggable: true,
-      dragVelocity: 1,
       arrows: {
         prev: '.glider-prev',
         next: '.glider-next'
       }
     })
-
-    glider.scrollItem(10)
-
+    
+    //changing week number
     document.querySelector('.glider').addEventListener('glider-slide-visible', function () {
       $('#weekNo')[0].innerHTML = this.querySelector('.active').getAttribute('data-week')
     })
   }
 
+  /**
+   * Scroll to current week
+   */
+  scrollToWeek() {
+    //scroll to current week
+    const firstLesson = new this.Lesson(this.lessons[0])
+    
+    this.glider.scrollItem( ($(`[data-date="${this.getToday()}"]`).parent().data('week') - firstLesson.getWeek()) * 5)
+  }
+  
+  /**
+   * Adding number of dates to date
+   * @param {*} date 
+   * @param {*} days 
+   * @returns String
+   */
   addDays(date, days) {
     date.setDate(date.getDate() + days)
-
+    
     const dd = String(date.getDate()).padStart(2, '')
     const mm = String(date.getMonth() + 1).padStart(2, '')
     const yyyy = date.getFullYear()
 
     return dd + '.' + mm + '.' + yyyy
   }
-
+  
+  /**
+   * Get current date as String
+   * @returns String
+   */
   getToday() {
     let today = new Date()
     const dd = String(today.getDate()).padStart(2, '')
     const mm = String(today.getMonth() + 1).padStart(2, '')
     const yyyy = today.getFullYear()
-
+    
     return dd + '.' + mm + '.' + yyyy
   }
-
+  
+  /**
+   * G
+   * @param {*} w 
+   * @param {*} y 
+   * @returns 
+   */
   getDateOfWeek(w, y) {
     let date = new Date(y, 0, (1 + w * 7))
     date.setDate(date.getDate() + (1 - date.getDay()))
     return date
   }
-
+  
+  /**
+   * Highlighting element which represents current day
+   */
   highlightToday() {
     $(`[data-date="${this.getToday()}"]`)[0].classList.add('today')
   }
-
+  
 }
--- WebSite/assets/js/script.js
+++ WebSite/assets/js/script.js
@@ -1,18 +1,38 @@
 import Table from './Table.js'
 import Lesson from './Lesson.js'
 
-$.ajax({
-  type: "GET",
-  url: "assets/lessons.xml",
-  dataType: "xml",
-  success: function(xml) {
-    init(xml)
-  }
+const table = new Table(null, Lesson)
+
+getData('lessonsX.xml')
+
+$('#classSelect').on('change', function (e) {
+  const option = this.options[this.selectedIndex].value
+  const fileName = 'lessons' + option + '.xml'
+
+  $('.schedule')[0].innerHTML = ''
+
+  getData(fileName)
 })
 
+function getData(fileName) {
+  $.ajax({
+    type: "GET",
+    url: "assets/" + fileName,
+    dataType: "xml",
+    success: function(xml) {
+      init(xml)
+    }
+  })
+}
+
+
 function init(xml) {
-  const table = new Table(xml, Lesson)
-  
+  table.xml = xml
+
+  if (table.glider != null ) {
+    console.log('a')
+    table.destroyGlider()
+  }
   table.generateTable()
 }
 
